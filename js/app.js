b422403e61e56f0a52e3a1409b899571ab13cded
nabil face <yampolos@gmail.com>
2016-10-20 17:27:19 +0200

    ajout de la liste users

--- js/app.js
+++ js/app.js
@@ -17,3 +17,18 @@ app.controller('depensesCtrl', function ($scope,$http){
       $scope.depenses = response.data.records;
    });
 });
+
+//afiche la modale filtrer
+app.controller("filtrerCtr", function($scope){
+    $scope.showContent = true;    
+});
+
+//transforme l'id du payeur en username
+app.controller('covertIdCtrl', function ($scope,$http){
+   $http({
+      method:"GET",
+      url:"json/users.json"
+   }).then(function(response){
+      $scope.name = response.data.records.username;
+   });
+});
